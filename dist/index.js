10c2f4da625d2c7fb79edda6e7617ff31a99845a
Kinrany <kinrany@yandex.com>
2019-10-25 04:00:59 +0300

    Add the code
    `index.js` contains a declaration for `hello()`, but the resulting type
    is `(name: any) => void` instead of `(name?: string) => void` anyway.

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(name) {
+    console.log("Hello " + (name !== null && name !== void 0 ? name : 'world'));
+}
+exports.hello = hello;
